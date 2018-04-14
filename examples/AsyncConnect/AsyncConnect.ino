0e3441a90ce73919ff485ae1a4139f1535f62183
ekampourakis <34644857+ekampourakis@users.noreply.github.com>
2018-04-14 19:42:38 +0300

    Default values

--- examples/AsyncConnect/AsyncConnect.ino
+++ examples/AsyncConnect/AsyncConnect.ino
@@ -4,27 +4,30 @@ EnhancedSerial ES;
 
 void setup() {
 	
-  //Initialize ES with an unique ID
-  ES.Init(0xB8);
+  // Initialize ES with the default unique ID
+  ES.Init(13);
   
 }
 
 void loop() {
   
-  //Try to connect asynchronously
-  //This needs to be called frequently 
-  //If ES is connected it won't do anything
+  // Try to connect asynchronously
+  // This needs to be called frequently 
+  // If ES is connected it will be bypassed
   ES.TryConnect();
 
   if (ES.IsConnected()) {
 	  
-    //You can call Serial methods as usual
+    // You can call Serial methods as usual
+	Serial.println("Hello World!");
+	delay(1000);
 	
   } else {
 	  
-    //As ES hasn't yet established a connection
-    //with the host, it's better to avoid calling
-    //any Serial methods until it connects
+    // As ES hasn't yet established a connection
+    // with the host, it's better to avoid calling
+    // any Serial methods until it connects.
+	// But you can do other crucial stuff here
 	
   }
   
--- examples/BlockingConnect/BlockingConnect.ino
+++ examples/BlockingConnect/BlockingConnect.ino
@@ -4,19 +4,19 @@ EnhancedSerial ES;
 
 void setup() {
 	
-  //Initialize ES with an unique ID
-  ES.Init(0xB8);
+  // Initialize ES with the default unique ID
+  ES.Init(13);
   
-  //Wait until ES connects to the host
+  // Wait until ES connects to the host
   ES.WaitConnect();
   
-  //You won't need to call Serial.begin() as EnhancedSerial initializes
-  //the Serial internally with the Baud rate specified by the host
+  // You won't need to call Serial.begin() as EnhancedSerial initializes
+  // the Serial internally with the Baud rate specified by the host
 }
 
 void loop() {
 	
-  //After initialization and connection you can use Serial functions as usual
+  // After initialization and connection you can use Serial functions as usual
   Serial.println("Hello World!");
   delay(1000);
   
